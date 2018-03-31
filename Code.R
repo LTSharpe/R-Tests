a1eb2d07e1b74b5ee3c34b21c560a2fcf15fad5d
LTSharpe <ak_public+github@icloud.com>
2018-03-31 17:35:54 +0200

    Processing of dynamic output, proper handling of missing nodes (via NA)

--- Code.R
+++ Code.R
@@ -26,42 +26,65 @@ remDr <- rD[["client"]]
 # navigate to page
 remDr$navigate(url)
 
-# get page html
-page_source <- read_html(remDr$getPageSource()[[1]])
+# press button to load more results until all are loaded
+chk <- FALSE
+page_number <- 0L
 
-# close RSelenium
+while(!chk){
+  # try to find button to load more results
+  button <- tryCatch(
+    remDr$findElement(using = 'css selector', ".pager-more__button"),
+    error = function(e){}
+  )
+  
+  if(length(button) > 0L) {
+  # press button if it's on page and wait for results to load
+    button$clickElement()
+    page_number <- page_number + 1L
+    print(paste0("Loading page #", page_number))
+    Sys.sleep(7L)
+  } else {
+  # if no more button on page exit loop
+    chk <- TRUE
+  }
+}
+
+# get page html
+page_source <- remDr$getPageSource()[[1]]
+page_html <- read_html(page_source)
+
+# close RSelenium & clean not needed objects from memory
 remDr$close()
 rD[["server"]]$stop()
-rm(rD)
+rm(rD, remDr, button, chk, page_number, page_source)
 
 # main nodes with product details
-nodes <- html_nodes(page_source, ".n-snippet-cell2")
+nodes <- html_nodes(page_html, ".n-snippet-cell2")
 
 # load data from web-page
 Data <- data.table(
   # product identifiers
-  ID = gsub("model-", "", html_attr(nodes, "data-id")),
+  ID = gsub("model-", "", nodes %>% html_attr("data-id")),
   # product titles
-  Title = html_nodes(nodes, ".n-snippet-cell2__title") %>% html_text(),
+  Title = nodes %>% html_node(".n-snippet-cell2__title") %>% html_text,
   # product main prices
   Main_Price = as.numeric(gsub(
-    "[^0-9]", "", html_nodes(nodes, ".n-snippet-cell2__main-price") %>% html_text()
+    "[^0-9]", "", nodes %>% html_node(".n-snippet-cell2__main-price") %>% html_text
     )),
   # product lowest prices
   Best_Price = as.numeric(gsub(
-    "[^0-9]", "", html_nodes(nodes, ".n-snippet-cell2__more-prices-link") %>% html_nodes(".price") %>% html_text()
+    "[^0-9]", "", nodes %>% html_node(".n-snippet-cell2__more-prices-link .price") %>% html_text
     )),
   # product number of quotes
   Quotes = as.numeric(gsub(
-    "[^0-9]", "", html_nodes(nodes, ".n-snippet-cell2__more-prices-link") %>% html_nodes(".link_type_prices") %>% html_text
+    "[^0-9]", "", nodes %>% html_node(".n-snippet-cell2__more-prices-link .link_type_prices") %>% html_text
     )),
   # product rating
   Rating = as.numeric(
-    html_nodes(nodes, ".rating__value") %>% html_text()
+    nodes %>% html_node(".rating__value") %>% html_text
     ),
   # product number of votes
   Votes = as.numeric(gsub(
-    "[^0-9]", "", html_nodes(nodes, ".n-snippet-card2__rating") %>% html_nodes("span") %>% html_text()
+    "[^0-9]", "", nodes %>% html_node(".n-snippet-card2__rating span") %>% html_text
     ))
 )
-     
